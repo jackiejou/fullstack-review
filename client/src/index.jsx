0040c55ee31396f8ce04d1caf9e5c34a6c2ef5a0
Jackie Jou <jakeic46@gmail.com>
2018-01-03 19:24:14 -0800

    error31

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,44 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import Search from './components/Search.jsx';
-import RepoList from './components/RepoList.jsx';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log(3214532);
-    this.state = {
-      repos: [],
-    };
-  }
-
-  search (term) {
-    console.log(`${term} was searched`);
-    $.ajax({
-      url: '/repos',
-      data: {username: term},
-      type: 'post',
-      error: (err) => {
-        console.log('error');
-      },
-      success: (data) => {
-        console.log('success');
-        // this.setState({repos: data});
-        $.ajax('/repos').done(results => {
-          this.setState({repos: results});
-        });
-      }
-    });
-  }
-
-  render () {
-    return (<div>
-      <h1>Github Fetcher</h1>
-      <Search onSearch={this.search.bind(this)}/>
-      <RepoList repos={this.state.repos}/>
-    </div>)
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import $ from 'jquery';
+// import Search from './components/Search.jsx';
+// import RepoList from './components/RepoList.jsx';
+//
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     console.log(3214532);
+//     this.state = {
+//       repos: [],
+//     };
+//   }
+//
+//   search (term) {
+//     console.log(`${term} was searched`);
+//     $.ajax({
+//       url: '/repos',
+//       data: {username: term},
+//       type: 'post',
+//       error: (err) => {
+//         console.log('error');
+//       },
+//       success: (data) => {
+//         console.log('success');
+//         // this.setState({repos: data});
+//         $.ajax('/repos').done(results => {
+//           this.setState({repos: results});
+//         });
+//       }
+//     });
+//   }
+//
+//   render () {
+//     return (<div>
+//       <h1>Github Fetcher</h1>
+//       <Search onSearch={this.search.bind(this)}/>
+//       <RepoList repos={this.state.repos}/>
+//     </div>)
+//   }
+// }
+//
+// ReactDOM.render(<App />, document.getElementById('app'));
